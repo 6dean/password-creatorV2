22b6f3183ad30c3804422363880608e8ee121420
6dean <ferdjallahrockdean@gmail.com>
2024-04-22 20:06:33 +0200

    password generator

--- src/app/password.js
+++ src/app/password.js
@@ -4,8 +4,8 @@ import { React, useState } from "react";
 export default function PasswordPage() {
   const [longer, setLonger] = useState(8);
   const [numbers, setNumbers] = useState(false);
-  const [letters, setLetters] = useState(false);
   const [specials, setSpecials] = useState(false);
+  const [password, setPassword] = useState("");
 
   const submitLonger = (event) => {
     setLonger(event.target.value);
@@ -15,26 +15,69 @@ export default function PasswordPage() {
     setNumbers(!numbers);
   };
 
-  const submitLetters = () => {
-    setLetters(!letters);
-  };
-
   const submitSpecials = () => {
     setSpecials(!specials);
   };
 
-  function generateRandomString() {
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    const numbers = "1234567890";
-    const specials = "!#$%&*+<=>?@{}~'";
+  const functionPass = () => {
+    if (numbers && specials) {
+      const generatePassword = () => {
+        const characters =
+          "A1B2C3D4E5F6G7H8I9J0K!L#M$N%O&P*Q+R<S=T>U?V@W{X}Y~Zabcdefghijklmnopqrstuvwxyz";
 
-    let result = "";
-    for (let i = 0; i < longer; i++) {
-      const randomIndex = Math.floor(Math.random() * characters.length);
-      result += characters[randomIndex];
+        let result = "";
+        for (let i = 0; i < longer; i++) {
+          const randomIndex = Math.floor(Math.random() * characters.length);
+          result += characters[randomIndex];
+        }
+        return setPassword(result);
+      };
+      generatePassword();
     }
-    return result;
-  }
+
+    if (specials && !numbers) {
+      const generatePassword = () => {
+        const characters =
+          "ABCDEFGHIJK!L#M$N%O&P*Q+R<S=T>U?V@W{X}Y~Zabcdefghijklmnopqrstuvwxyz";
+
+        let result = "";
+        for (let i = 0; i < longer; i++) {
+          const randomIndex = Math.floor(Math.random() * characters.length);
+          result += characters[randomIndex];
+        }
+        return setPassword(result);
+      };
+      generatePassword();
+    }
+    if (!specials && numbers) {
+      const generatePassword = () => {
+        const characters =
+          "A1B2C3D4E5F6G7H8I9J0K1L2M3N4O5P6Q7R8S9T0U1V2W3X4Y5Z6abcdefghijklmnopqrstuvwxyz1234567890";
+
+        let result = "";
+        for (let i = 0; i < longer; i++) {
+          const randomIndex = Math.floor(Math.random() * characters.length);
+          result += characters[randomIndex];
+        }
+        return setPassword(result);
+      };
+      generatePassword();
+    } else if (!specials && !numbers) {
+      console.log("special is fake / numbers is fake");
+      const generatePassword = () => {
+        const characters =
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        let result = "";
+        for (let i = 0; i < longer; i++) {
+          const randomIndex = Math.floor(Math.random() * characters.length);
+          result += characters[randomIndex];
+        }
+        return setPassword(result);
+      };
+      generatePassword();
+    }
+  };
 
   return (
     <div>
@@ -57,17 +100,15 @@ export default function PasswordPage() {
         </div>
       </div>
       <div>
-        <div>Mettre des lettres</div>
-        <div>
-          <input type="checkbox" checked={letters} onChange={submitLetters} />
-        </div>
-      </div>
-      <div>
         <div>Mettre des caractères spéciaux</div>
         <div>
           <input type="checkbox" checked={specials} onChange={submitSpecials} />
         </div>
       </div>
+      <div>
+        <input type="button" onClick={functionPass} />
+      </div>
+      <div>{password}</div>
     </div>
   );
 }
